6040cd823f612c74abcbde30a207e3aab77b45f4
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-05-22 12:42:08 -0700

    cleaning up temporary files

--- client.sh
+++ client.sh
@@ -3,11 +3,21 @@
 #Usage: ./client.sh <server hostname> <server listening port>
 #Example: ./client.sh molluska.cs.uoregon.edu 12345
 
+exit_funct()
+{
+  echo "Exiting..."
+  rm -f $TMP_FILE_1
+  rm -f $TMP_FILE_2
+  exit 0
+}
+
 TMP_FILE_1=$(date +%s | shasum | tr ' -' 'X')
 touch $TMP_FILE_1
 sleep 1
 TMP_FILE_2=$(date +%s | shasum | tr ' -' 'X')
 touch $TMP_FILE_2
+
+trap exit_funct SIGINT
 
 while true
 do
